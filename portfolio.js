28db88ae78eb1e0904ee67a13a51a6cba0973a4d
anniebehm <anniebehm444@gmail.com>
2017-07-20 15:45:34 -0400

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+$("h1").click(function(){
+    alert("You have found a secret fun fact! Annie's birthday is on October 30th!");
+});
+
+$("img").click(function(){
+    alert("You have found a secret fun fact! Annie has two siblings!");
+});
