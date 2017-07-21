e39a44a6e59683949fe16cfb7f4157614f8b98db
anniebehm <anniebehm444@gmail.com>
2017-07-21 10:13:49 -0400

    Create about.js

---
+++ about.js
@@ -0,0 +1,40 @@
+$("h1").click(function(){
+    alert("Make sure to click the questions for answers! Not the headings! :)");
+});
+$(".Q1").click(function(){
+    alert("Annie's favorite color is blue or teal!");
+});
+$(".Q2").click(function(){
+    alert("Annie is on the ski team and she likes to run and jump over hurdles!");
+});
+   $(".Q3").click(function(){
+    alert("Grand Blanc High School.");
+});
+   $(".Q4").click(function(){
+    alert("Annie has 2 siblings! One brother, named Jack, and one sister, named Ellie!");
+});
+$(".Q5").click(function(){
+    alert("Annie loves all types of water, swimming, rain and just about everything else! (She also really loves candy.)");
+});
+$(".Q6").click(function(){
+    alert("For some reason she really likes the number 4.");
+});
+$(".Q7").click(function(){
+    alert("Annie really likes the fourth Harry Potter movie, she also really loves the movie The Internship.");
+});
+$(".Q8").click(function(){
+    alert("Annie was born in Flint Michigan, on October 30th 2003!");
+});
+$(".Q9").click(function(){
+    alert("Her full name is Ann Lousie Behm! :)");
+});
+$(".Q10").click(function(){
+    alert("Annie is about 5'6! (And still growing.)");
+});
+   
+   
+   
+   
+   
+   
+   
