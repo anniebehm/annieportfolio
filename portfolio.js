38a8073282386ed33654488d6a66170e9b314a9e
anniebehm <anniebehm444@gmail.com>
2017-07-21 11:33:00 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,22 @@
+$("#all").hide();
+
 $("h1").click(function(){
     alert("You have found a secret fun fact! Annie's birthday is on October 30th!");
 });
 
 $("img").click(function(){
-    alert("You have found a secret fun fact! Annie has two siblings!");
+    alert("You have found a secret fun fact! Annie has two siblings!"); });
+
+$("#guess_button").click(function() {
+    var input = $("#inputbox").val();
+    var magicnumber = "seal";
+    
+    if (input === magicnumber){$("#all").show();
+    $("p").html("Congrats!");
+    $("#login").hide();
+    }
+    
+    else if (input !== magicnumber){
+        $("p").html("Nope! My favorite animals are seals! Try again! :)");
+    }
 });
